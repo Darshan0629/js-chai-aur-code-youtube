b39904697064569a0b0e54acb38284987d567586
Darshan Mangrola <121339364+Darshan0629@users.noreply.github.com>
2024-10-13 21:36:21 +0000

    add memory part to file

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -14,7 +14,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -34,6 +34,33 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (Primitive) and heap (Non-primitive)
+
+// in stack we share a copy and in heap we share a reference 
+
+let firstName = "Darshan"
+
+let fullName = firstName // share a copy of firstName
+fullName = "Darshan Mangrola" // change the fullName
+
+// console.log(firstName);
+// console.log(fullName); // fullName is changed but firstName is remains same.
+
+let userOne = {
+    email: "darshanmangrola@gmail.com",
+    upi: "darshan@bob"
+}
+
+let userTwo = userOne // share same reference
+
+userTwo.email = "darshan@gamil.com" // change the email of userTwo
+
+console.log(userOne.email) // email of userOne is also changed bcz both share same reference
+console.log(userTwo.email)
